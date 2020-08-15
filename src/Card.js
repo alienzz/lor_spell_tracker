62a76d7a690d6dd11d8b156274e091b8c1c93720
Michael Myers <michaelzmyers@gmail.com>
2020-08-15 00:03:58 -0700

    updated image urls to http with a dorky replace

--- src/Card.js
+++ src/Card.js
@@ -2,11 +2,12 @@ import React from 'react'
 import {FlexboxGrid} from 'rsuite'
 
 const Card = ({ data }) => {
+    const url = data.assets[0].gameAbsolutePath.replace('http', 'https')
     return (
         <FlexboxGrid.Item>
             <img 
                 style={{height: 300}}
-                src={data.assets[0].gameAbsolutePath} 
+                src={url} 
                 alt={data.name}
             />
         </FlexboxGrid.Item>
